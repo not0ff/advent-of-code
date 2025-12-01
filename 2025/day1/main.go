79e85196f5ceaeaa8dd7693a450e0641b038d337
not0ff <209936853+not0ff@users.noreply.github.com>
2025-12-01 23:31:47 +0100

    part1

---
+++ 2025/day1/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func moveRight(v int, d int) int {
+	for range d {
+		if v+1 <= 99 {
+			v++
+		} else {
+			v = 0
+		}
+	}
+	return v
+}
+
+func moveLeft(v int, d int) int {
+	for range d {
+		if v-1 >= 0 {
+			v--
+		} else {
+			v = 99
+		}
+	}
+	return v
+}
+
+func part1(input *bufio.Scanner) string {
+	count, value := 0, 50
+	for input.Scan() {
+		seq := strings.Split(input.Text(), "")
+		distance, _ := strconv.Atoi(strings.Join(seq[1:], ""))
+		switch seq[0] {
+		case "L":
+			value = moveLeft(value, distance)
+		case "R":
+			value = moveRight(value, distance)
+		}
+		if value == 0 {
+			count++
+		}
+	}
+	return fmt.Sprintf("Password: %v", count)
+}
+
+var (
+	Part  = flag.String("part", "p1", "")
+	Input = flag.String("input", "inputs/input.txt", "")
+)
+
+func main() {
+	flag.Parse()
+	f, err := os.Open(*Input)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer f.Close()
+	s := bufio.NewScanner(f)
+
+	switch *Part {
+	case "p1":
+		fmt.Println(part1(s))
+	}
+}
