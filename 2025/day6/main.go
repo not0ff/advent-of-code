363b9b087be1f58c4cde349305f6b16d885a0775
not0ff <209936853+not0ff@users.noreply.github.com>
2025-12-06 13:44:35 +0100

    part 2 w/ magic for loops

--- 2025/day6/main.go
+++ 2025/day6/main.go
@@ -9,23 +9,74 @@ import (
 	"strings"
 )
 
-func applyOperators(nums [][]int, ops []string) []int {
-	res := make([]int, 0, len(nums))
+func applyOp(res *int, n int, op string) {
+	switch op {
+	case "+":
+		*res += n
+	case "*":
+		if *res == 0 {
+			*res = 1
+		}
+		*res *= n
+	}
+}
+
+func part1(lines [][]string) []int {
+	nums := make([][]int, 0)
+	ops := make([]string, 0)
+	for _, l := range lines {
+		s := strings.Join(l, "")
+		if strings.ContainsAny(s, "+*") {
+			ops = strings.Fields(s)
+			break
+		}
+
+		for i, n := range strings.Fields(s) {
+			if len(nums)-1 < i {
+				nums = append(nums, make([]int, 0, len(lines)-1))
+			}
+			num, _ := strconv.Atoi(n)
+			nums[i] = append(nums[i], num)
+		}
+	}
+	res := make([]int, len(ops))
 	for i, op := range ops {
 		for _, n := range nums[i] {
-			switch op {
-			case "+":
-				if len(res)-1 < i {
-					res = append(res, 0)
-				}
-				res[i] += n
-			case "*":
-				if len(res)-1 < i {
-					res = append(res, 1)
-				}
-				res[i] *= n
-			}
+			applyOp(&res[i], n, op)
 		}
+	}
+	return res
+}
+
+func part2(lines [][]string) []int {
+	// Assumes that lines have equal length
+	res := make([]int, 0)
+	resN := 0
+	var op string
+	for i := range lines[0] {
+		if s := lines[len(lines)-1][i]; s != " " {
+			op = s
+		}
+
+		r := ""
+		for _, arr := range lines[:len(lines)-1] {
+			if arr[i] == " " {
+				continue
+			}
+			r += arr[i]
+		}
+
+		if r == "" {
+			resN++
+			continue
+		}
+
+		if len(res)-1 < resN {
+			res = append(res, 0)
+		}
+
+		n, _ := strconv.Atoi(r)
+		applyOp(&res[resN], n, op)
 	}
 	return res
 }
@@ -42,27 +93,20 @@ func main() {
 	defer f.Close()
 
 	s := bufio.NewScanner(f)
-	numsArr := make([][]int, 0)
-	res := make([]int, 0)
-	for i := 0; s.Scan(); i++ {
-		t := s.Text()
-		if strings.ContainsAny(t, "+*") {
-			res = applyOperators(numsArr, strings.Fields(t))
-			break
-		}
-
-		for j, n := range strings.Fields(t) {
-			if len(numsArr)-1 < j {
-				numsArr = append(numsArr, make([]int, 0))
-			}
-			num, _ := strconv.Atoi(n)
-			numsArr[j] = append(numsArr[j], num)
-		}
+	lines := make([][]string, 0)
+	for s.Scan() {
+		lines = append(lines, strings.Split(s.Text(), ""))
 	}
 
-	sum := 0
-	for _, n := range res {
-		sum += n
+	sum1 := 0
+	for _, n := range part1(lines) {
+		sum1 += n
 	}
-	fmt.Println("Answer:", sum)
+
+	sum2 := 0
+	for _, n := range part2(lines) {
+		sum2 += n
+	}
+	fmt.Println("Part 1:", sum1)
+	fmt.Println("Part 2:", sum2)
 }
